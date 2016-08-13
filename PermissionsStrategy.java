49f1e269458eb0981ad5deaad3d2b6e22ab14dcd
Thomas Brüggemann <mail@thomasbrueggemann.com>
2016-08-14 00:52:43 +0200

    weiterarbeit listings

--- PermissionsStrategy.java
+++ PermissionsStrategy.java
@@ -1,13 +1,11 @@
 String manifest = this.app.getManifestFile();
-if (manifest != null) {
-	FileScanner scanner = new FileScanner(manifest);
-	LinkedList<Snippet> snippets = new LinkedList<Snippet>();
-	for (String searchTerm : (LinkedList<String>) this.params.get("searchFor")) {
-		try {
-			snippets.addAll(scanner.scan(searchTerm));
-		} catch (FileNotFoundException e) {}
-	}
-	if (snippets.size() > 0) {
-		return new StrategyResult(StrategyResultProbability.HIGH, true, snippets);
-	}
+FileScanner scanner = new FileScanner(manifest);
+LinkedList<Snippet> snippets = new LinkedList<Snippet>();
+for (String searchTerm : (LinkedList<String>) this.params.get("searchFor")) {
+	try {
+		snippets.addAll(scanner.scan(searchTerm));
+	} catch (FileNotFoundException e) {}
+}
+if (snippets.size() > 0) {
+	return new StrategyResult(StrategyResultProbability.HIGH, true, snippets);
 }
